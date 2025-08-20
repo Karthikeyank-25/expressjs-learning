b921763f24be92fee325d1c1795cee630f97003c
Karthikeyank-25 <karthikeyankandan25@gmail.com>
2025-08-20 17:06:04 +0530

    ExpressJs

---
+++ expressjs.js
@@ -0,0 +1,11 @@
+// ExpressJs :
+//          Express.js is a minimal and flexible Node.js web application framework that provides a set of features to build web and mobile applications. 
+//          It’s one of the most popular frameworks for Node.js because it simplifies the process of building server-side applications.
+
+// Features of Express.js:
+//          - Fast & Lightweight – Built on top of Node.js.
+//          - Routing – Provides powerful routing for handling different URLs.
+//          - Middleware support – Lets you handle requests, responses, and errors.
+//          - Template engines – Supports engines like EJS, Pug, and Handlebars.
+//          - HTTP utilities – Easy handling of requests, responses, cookies, and sessions.
+//          - REST API support – Commonly used for building APIs.
